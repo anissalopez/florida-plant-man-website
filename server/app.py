b2e57dca4d418daa48849f493bd62292118f562c
anissalopez <anissa965@gmail.com>
2023-12-14 10:50:34 -0500

    added plant model + seed data

--- server/app.py
+++ server/app.py
@@ -3,12 +3,14 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+import json
+from flask import make_response, request
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import Plant
 
 
 # Views go here!
@@ -17,6 +19,47 @@ from config import app, db, api
 def index():
     return '<h1>Project Server</h1>'
 
+class Plants(Resource):
+    def get(self):
+        plants = [plant.to_dict for plant in Plant.query.all()]
+
+        response = make_response(plants, 200)
+        return response
+    
+    
+
+    def post(self):
+        data = request.get_json()
+
+        try:
+            name = data['name']
+            price = data['price']
+            care_instructions = data.get('care_instructions')
+            qty = data['qty']
+            images = data['images']
+
+            # Create a new Plant object
+            new_plant = Plant(
+                name=name,
+                price=price,
+                care_instructions=care_instructions,  # No need to serialize if it's already JSON
+                qty=qty,
+                images=images
+            )
+
+            # Add the new plant to the database session and commit
+            db.session.add(new_plant)
+            db.session.commit()
+
+            # Return a success response with the created plant data
+            response_data = make_response(new_plant.to_dict(), 201)
+            return response_data
+        except Exception as exc:
+            response = make_response({"An error occurred": exc}, 400)
+            return response
+
+
+api.add_resource(Plants, '/plants')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,32 @@
+from sqlalchemy import DateTime, func
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
 # Models go here!
+class Plant(db.Model, SerializerMixin):
+    __tablename__ ='plants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    water = db.Column(db.String)
+    sun = db.Column(db.String)
+    qty = db.Column(db.Integer)
+    price = db.Column(db.String)
+    image1 = db.Column(db.String)
+    image2 = db.Column(db.String)
+    image3 = db.Column(db.String)
+    created_at = db.Column(DateTime(), server_default=func.now())
+    updated_at = db.Column(DateTime(), onupdate=func.now())
+
+    @validates('name', 'care_instructions', 'images')
+    def validate_inputs(self, key, value):  # Use 'key' to access the field name, and 'value' for its value
+        if not len(value):
+            raise ValueError('Please enter a valid input')
+        return value
+    
+
+    def __repr__(self):
+        return f'Id: {self.id} - Name: {self.name} - Qty:{self.qty} - price:{self.price}'
