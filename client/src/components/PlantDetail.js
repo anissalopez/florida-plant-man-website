2e47c6555b07761f201469c36324e99bf746ef23
anissalopez <anissa965@gmail.com>
2023-12-30 11:34:50 -0500

    working on plantDetail component

--- client/src/components/PlantDetail.js
+++ client/src/components/PlantDetail.js
@@ -1,13 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-
 import  Card  from "@mui/material/Card";
-import  CardMedia  from "@mui/material/CardMedia"
+import  CardMedia  from "@mui/material/CardMedia";
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 
 
@@ -15,6 +14,7 @@ function PlantDetail({}) {
   const { id } = useParams();
   const [plant, setPlant] = useState(null);
   const [displayImg, setDisplayImg] = useState(plant ? plant.image1 : null)
+  const [plantImages, setPlantImages] = useState([])
 
   useEffect(() => {
     const fetchPlant = async () => {
@@ -23,6 +23,7 @@ function PlantDetail({}) {
           if (!response.ok) throw Error('Error receiving data')
           const plantData = await response.json()
           setPlant(plantData)
+          setPlantImages([plantData.image1, plantData.image2, plantData.image3])
           setDisplayImg(plantData.image1)
           // setFetchError(null)
       }catch(err){
@@ -34,67 +35,48 @@ function PlantDetail({}) {
     }
     fetchPlant()
   }, [id]);
-
+  console.log(plant.name)
 
   
   return (
     <>
     {plant ? 
       <Grid container  spacing={0}>
-        <Box  sx={{width:"300px",
-                  marginLeft:"40px", border:1}} xs={4}>
+        <Box  sx={{marginLeft:"40px", border:1}} xs={4}>
+          <Card  sx={{backgroundColor: "#f1f1f1"}}>
+            {plantImages.map((image) => (
+              <CardMedia component="img" alt="featured plant" image={image} key={image}
+                          sx={{
+                                height:"200px",
+                                padding:"10px",
+                            }}/>
+            ))}      
+          </Card>
+       </Box>
+       <Box sx={{border:1}} xs={4}>
+          <Card sx={{backgroundColor: "#f1f1f1"}}>
+            <CardMedia
+                        component="img"
+                        alt="featured plant"
+                        image={plant.image1}
+                        sx={{
+                            height:"600px",
+                            padding: "10px"
+                        }}
+                    />
+            </Card>
+       </Box>
+       <Box>
         <Card>
-        <CardMedia component="img" alt="featured plant" image={plant.image1}
-                   sx={{
-                        height:"200px",
-                        padding:"10px"
-                    }}
-                />
-                 <CardMedia
-                    component="img"
-                    alt="featured plant"
-                    image={plant.image1}
-                    sx={{
-                        height:"200px",
-                        padding:"10px"
-                    }}
-                />
-                 <CardMedia
-                    component="img"
-                    alt="featured plant"
-                    image={plant.image1}
-                    sx={{
-                        height:"200px",
-                        padding:"10px"
-                    }}
-                />
+          <CardContent>
+            <Typography variant="h1">
+              {plant.name}
+            </Typography>
+          </CardContent>
         </Card>
-       
-      {/* </Grid> */}
-      </Box>
-      <Box sx={{border:1}} xs={4}>
-      <Card>
-        <CardMedia
-                    component="img"
-                    alt="featured plant"
-                    image={plant.image1}
-                    sx={{
-                        height:"600px",
-                        padding: "10px"
-                      
-                    }}
-                />
-        </Card>
- 
-  
-      {/* </Grid> */}
-      </Box>
-
+       </Box>
       </Grid>
-     
-     
      : null}
-
       </>
   );
 }
