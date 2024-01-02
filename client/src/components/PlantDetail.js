1809576c3b2c40e196f12f44bcde3164e5b8456d
anissalopez <anissa965@gmail.com>
2024-01-02 17:09:08 -0500

    added more formatting to plant detail page

--- client/src/components/PlantDetail.js
+++ client/src/components/PlantDetail.js
@@ -1,7 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import * as React from 'react';
-import styles from "../styles/PlantDetail.module.css"
+import styles from "../styles/PlantDetail.module.css";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import StraightenIcon from '@mui/icons-material/Straighten';
 
 
 
@@ -9,8 +12,6 @@ function PlantDetail({ plants }) {
   const { id } = useParams();
   const [plant, setPlant] = useState('');
  
-
-
   useEffect(() => {
     const fetchPlant = async () => {
       try{
@@ -21,7 +22,6 @@ function PlantDetail({ plants }) {
           console.log(plantData)
           setPlant(plantData)
           console.log(plant)
-        
           // setFetchError(null)
       }catch(err){
           // setFetchError(err.message)
@@ -39,13 +39,35 @@ function PlantDetail({ plants }) {
     {plant ? 
      <div className={styles.container}>
           <div className={styles.sideleft}>
-            <div style={{background: `url(${plant.image1})`, backgroundSize: "100% 100%"}}>1</div>
-            <div style={{background: `url(${plant.image2})`, backgroundSize: "100% 100%"}}>2</div>
-            <div style={{background: `url(${plant.image3})`, backgroundSize: "100% 100%"}}>3</div>
+            <div style={{background: `url(${plant.image1})`, backgroundSize: "100% 100%"}}></div>
+            <div style={{background: `url(${plant.image2})`, backgroundSize: "100% 100%"}}></div>
+            <div style={{background: `url(${plant.image3})`, backgroundSize: "100% 100%"}}></div>
         </div>
         <div className={styles.main}>
-            <div style={{background: `url(${plant.image1})`, backgroundSize: "100% 100%", padding:"10px"}}>4</div>
+            <div style={{background: `url(${plant.image1})`, backgroundSize: "100% 100%", padding:"10px"}}></div>
           </div>
+        <div className={styles.plantInfo}>
+          <h3>{plant.name}</h3>
+        
+          <div>
+            <WbSunnyIcon />
+            <p>{plant.sun}</p>
+          </div>
+          <div>
+            <WaterDropIcon />
+            <p>{plant.water}</p>
+          </div>
+          <div>
+            <StraightenIcon />
+          </div>
+        
+       
+          <p>{plant.price}</p>
+          <button>Add to Cart</button>
+     
+          
+
+        </div>
      </div>
       
      : null}
